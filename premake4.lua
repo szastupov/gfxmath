49551fcc57b884786a8e9960959b8c44935c6d10
Stepan Zastupov <stepan.zastupov@gmail.com>
2012-11-28 15:46:58 +0400

    remove CMakeLists.txt

--- premake4.lua
+++ premake4.lua
@@ -1,5 +1,2 @@
-project "gfxmath"
-        kind "StaticLib"
-        language "C++"
-        files { "**.h", "**.cpp" }
-        excludes { "math_test.cpp" }
+files { "**.h", "**.cpp" }
+excludes { "math_test.cpp" }
